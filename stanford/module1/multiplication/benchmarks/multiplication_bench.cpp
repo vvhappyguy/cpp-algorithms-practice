d2b08404fbbbf13b6956665a25386b67fb4a8d9a
Ivan Dyakin <vvhappyguy@gmail.com>
2026-05-04 20:13:59 +0400

    Added assessment data

--- stanford/module1/multiplication/benchmarks/multiplication_bench.cpp
+++ stanford/module1/multiplication/benchmarks/multiplication_bench.cpp
@@ -76,6 +76,19 @@ void run_benchmark(const std::string& name, multiplication_fn f,
 }
 
 int main() {
+  // assessment 
+  {
+    digit_list n1 = {3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3,2,7,9,5,0,2,8,8,4,1,9,7,1,6,9,3,9,9,3,7,5,1,0,5,8,2,0,9,7,4,9,4,4,5,9,2};
+    digit_list n2 = {2,7,1,8,2,8,1,8,2,8,4,5,9,0,4,5,2,3,5,3,6,0,2,8,7,4,7,1,3,5,2,6,6,2,4,9,7,7,5,7,2,4,7,0,9,3,6,9,9,9,5,9,5,7,4,9,6,6,9,6,7,6,2,7};
+
+
+    digit_list n = grade_school_product(n1, n2);
+    for(auto l: n)
+    {
+      std::cout << l;
+    }
+  }
+
   const std::vector<size_t> sizes = {
       1, 2, 4, 8, 16, 32, 64, 128, 256, 512,
   };
